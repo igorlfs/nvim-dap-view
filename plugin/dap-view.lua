c13d296879f83ef0b97886678c54ed29b1cc8f9a
Igor <igorlfs@ufmg.br>
2025-02-12 19:26:37 -0300

    prefer bang instead of new command

--- plugin/dap-view.lua
+++ plugin/dap-view.lua
@@ -1,7 +1,12 @@
 local command = vim.api.nvim_create_user_command
 
 command("DapViewOpen", require("dap-view").open, {})
-command("DapViewClose", require("dap-view").close, {})
-command("DapViewHide", require("dap-view").hide, {})
+command("DapViewClose", function(opts)
+    if opts.bang then
+        require("dap-view").hide()
+    else
+        require("dap-view").close()
+    end
+end, { bang = true })
 command("DapViewToggle", require("dap-view").toggle, {})
 command("DapViewWatch", require("dap-view").add_expr, {})
