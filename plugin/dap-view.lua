30cf0e3c9ad9a33838de7ec63e7c1aaab09dad96
Igor <igorlfs@ufmg.br>
2025-02-14 23:45:23 -0300

    final cleanup

--- plugin/dap-view.lua
+++ plugin/dap-view.lua
@@ -2,17 +2,9 @@ local command = vim.api.nvim_create_user_command
 
 command("DapViewOpen", require("dap-view").open, {})
 command("DapViewClose", function(opts)
-    if opts.bang then
-        require("dap-view").close(true)
-    else
-        require("dap-view").close()
-    end
+    require("dap-view").close(opts.bang)
 end, { bang = true })
 command("DapViewToggle", function(opts)
-    if opts.bang then
-        require("dap-view").toggle(true)
-    else
-        require("dap-view").toggle()
-    end
+    require("dap-view").toggle(opts.bang)
 end, { bang = true })
 command("DapViewWatch", require("dap-view").add_expr, {})
