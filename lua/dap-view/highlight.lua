235f701073fd9603570c8164106e70cfdb55b2da
Igor <igorlfs@ufmg.br>
2025-04-06 11:13:50 -0300

    fix(hl): reload base links on updating colorscheme

--- lua/dap-view/highlight.lua
+++ lua/dap-view/highlight.lua
@@ -6,13 +6,21 @@ local hl_create = function(name, link)
     api.nvim_set_hl(0, "NvimDapView" .. name, { link = link })
 end
 
-hl_create("MissingData", "DapBreakpoint")
-hl_create("WatchText", "Comment")
-hl_create("WatchTextChanged", "DiagnosticVirtualTextWarn")
-hl_create("ExceptionFilterEnabled", "DiagnosticOk")
-hl_create("ExceptionFilterDisabled", "DiagnosticError")
-hl_create("FileName", "qfFileName")
-hl_create("LineNumber", "qfLineNr")
-hl_create("Separator", "Comment")
-hl_create("Thread", "@namespace")
-hl_create("ThreadStopped", "@conditional")
+local define_base_links = function()
+    hl_create("MissingData", "DapBreakpoint")
+    hl_create("WatchText", "Comment")
+    hl_create("WatchTextChanged", "DiagnosticVirtualTextWarn")
+    hl_create("ExceptionFilterEnabled", "DiagnosticOk")
+    hl_create("ExceptionFilterDisabled", "DiagnosticError")
+    hl_create("FileName", "qfFileName")
+    hl_create("LineNumber", "qfLineNr")
+    hl_create("Separator", "Comment")
+    hl_create("Thread", "@namespace")
+    hl_create("ThreadStopped", "@conditional")
+end
+
+define_base_links()
+
+api.nvim_create_autocmd("ColorScheme", {
+    callback = define_base_links,
+})
