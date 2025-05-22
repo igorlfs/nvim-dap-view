823c8683c82cd149102e97c500cc93c1b6964186
Igor <igorlfs@ufmg.br>
2025-05-21 21:58:48 -0300

    Updates

--- _app/immutable/chunks/BbJzynV6.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_102f2be)==null?void 0:s.base)??"/igorlfs.github.io/nvim-dap-view";var e;const t=((e=globalThis.__sveltekit_102f2be)==null?void 0:e.assets)??a;export{t as a,a as b};
