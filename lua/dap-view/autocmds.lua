4f279cee2d421ce103506c86f985a3a45878991e
Igor <igorlfs@ufmg.br>
2025-09-08 23:09:53 -0300

    fix: handle switching buffers and tabs in a less hacky manner

--- lua/dap-view/autocmds.lua
+++ lua/dap-view/autocmds.lua
@@ -22,96 +22,60 @@ api.nvim_create_autocmd("TabEnter", {
             require("dap-view.actions").open(state.term_winnr ~= nil)
         end
 
-        local winnr = nil
-        local term_winnr = nil
+        -- Defer execution to ensure that the filetype doesn't match the one that created the tab
+        --
+        -- Otherwise the new window may get assigned as the `state.winnr` (if the tab was created from
+        -- nvim-dap-view's window, which is possible when `switchbuf` contains "newtab")
+        --
+        -- This can also happen if regular nvim-dap's switchbuf contains "newtab"
+        --
+        -- When a new tab is created, for a brief moment the new window inherits the filetype of the window
+        -- that the user was on when `tabnew` was called
+        --
+        -- When a random window gets assigned as `state.winnr` all sorts of crazy stuff may happen.
+        -- However, most notably, the window will inherit nvim-dap-view's winbar
+        vim.schedule(function()
+            local winnr = nil
+            local term_winnr = nil
 
-        for _, win in ipairs(api.nvim_tabpage_list_wins(0)) do
-            local bufnr = api.nvim_win_get_buf(win)
-            local ft = vim.bo[bufnr].filetype
+            for _, win in ipairs(api.nvim_tabpage_list_wins(0)) do
+                local bufnr = api.nvim_win_get_buf(win)
+                local ft = vim.bo[bufnr].filetype
 
-            if ft == "dap-view" then
-                if winnr == nil then
-                    winnr = win
-                end
-            end
-
-            if ft == "dap-view-term" then
-                if state.current_section == "console" then
+                if ft == "dap-view" then
                     if winnr == nil then
                         winnr = win
                     end
-                elseif term_winnr == nil then
-                    term_winnr = win
+                end
+
+                if ft == "dap-view-term" then
+                    if state.current_section == "console" then
+                        if winnr == nil then
+                            winnr = win
+                        end
+                    elseif term_winnr == nil then
+                        term_winnr = win
+                    end
+                end
+
+                if ft == "dap-repl" and state.current_section == "repl" then
+                    if winnr == nil then
+                        winnr = win
+                    end
                 end
             end
 
-            if ft == "dap-repl" and state.current_section == "repl" then
-                if winnr == nil then
-                    winnr = win
-                end
+            state.winnr = winnr
+            -- Track the state of the last term win, so it can be closed later if becomes a leftover window
+            if state.term_winnr and term_winnr ~= state.term_winnr then
+                state.last_term_winnr = state.term_winnr
             end
-        end
+            state.term_winnr = term_winnr
 
-        state.winnr = winnr
-        -- Track the state of the last term win, so it can be closed later if becomes a leftover window
-        if state.term_winnr and term_winnr ~= state.term_winnr then
-            state.last_term_winnr = state.term_winnr
-        end
-        state.term_winnr = term_winnr
-
-        if winnr ~= nil then
-            winbar.show_content(state.current_section)
-        end
-    end,
-})
-
-api.nvim_create_autocmd("BufEnter", {
-    callback = function(args)
-        -- We need to get rid of this mess ASAP 💀
-        local buf = args.buf
-        if not api.nvim_buf_is_valid(buf) then
-            return
-        end
-        local win = api.nvim_get_current_win()
-        local ft = vim.bo[buf].filetype
-
-        local winbar_ = setup.config.winbar
-        local is_allowed_custom_ft = vim.iter(winbar_.custom_sections):any(
-            ---@param section string
-            function(section)
-                local custom = winbar_.custom_sections[section]
-                return custom and custom.filetype == ft
+            if winnr ~= nil then
+                winbar.show_content(state.current_section)
             end
-        )
-
-        -- Reset the winnr if the buffer changed
-        --
-        -- We can't use winfixbuf since the window shares many buffers (REPL, Console)
-        --
-        -- Therefore, it's possible to switch to a (regular) buffer (any ft) while keeping the status of state.winnr
-        --
-        -- While it's unlikely users do that very often, such change occurs when the switchbuf is triggered as "newtab"
-        -- For some reason, the new tab starts with the "dap-view" ft, which causes the winbar to appear on the regular buffer
-        --
-        -- By the way, we also have to take custom filetypes into account, as they belong in the winnr
-        if state.winnr == win then
-            if not vim.tbl_contains({ "dap-view", "dap-view-term", "dap-repl" }, ft) and not is_allowed_custom_ft then
-                state.winnr = nil
-            end
-        elseif not state.winnr then
-            if
-                vim.tbl_contains({ "dap-view", "dap-repl" }, ft)
-                or (ft == "dap-view-term" and state.current_section == "console")
-                or is_allowed_custom_ft
-            then
-                state.winnr = win
-            end
-        end
-
-        -- For good measure, also handle term_winnr
-        if state.term_winnr == win and ft ~= "dap-view-term" then
-            state.term_winnr = nil
-        end
+        end)
     end,
 })
 
