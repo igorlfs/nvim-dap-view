4a7f4069d65ca10b4bd4f4bd2ba4febb27ac921e
runie <5571284+Run1e@users.noreply.github.com>
2025-09-09 09:17:43 +0200

    fix: fix autoscroll in case of early terminal focus

--- lua/dap-view/console/scroll.lua
+++ lua/dap-view/console/scroll.lua
@@ -27,24 +27,29 @@ M.scroll = function(bufnr)
 
     api.nvim_buf_attach(bufnr, false, {
         on_lines = function()
-            local winnr = vim.tbl_contains(setup.config.winbar.sections, "console") and state.winnr or state.term_winnr
-            if not util.is_win_valid(winnr) then
-                return
-            end
-            ---@cast winnr integer
-            if autoscroll and vim.fn.mode() == "n" then
-                api.nvim_win_call(winnr, function()
-                    if api.nvim_get_current_buf() == bufnr then
-                        -- vim.schedule ensures the cursor movement happens in the main event loop
-                        -- otherwise the call may happen too early
-                        vim.schedule(function()
-                            api.nvim_win_set_cursor(winnr, { api.nvim_buf_line_count(bufnr), 0 })
-                        end)
-                    end
-                end)
-            end
+            M.setup_buf(bufnr)
         end,
     })
+end
+
+---@param bufnr integer
+M.setup_buf = function(bufnr)
+    local winnr = vim.tbl_contains(setup.config.winbar.sections, "console") and state.winnr or state.term_winnr
+    if not util.is_win_valid(winnr) then
+        return
+    end
+    ---@cast winnr integer
+    if autoscroll and vim.fn.mode() == "n" then
+        api.nvim_win_call(winnr, function()
+            if api.nvim_get_current_buf() == bufnr then
+                -- vim.schedule ensures the cursor movement happens in the main event loop
+                -- otherwise the call may happen too early
+                vim.schedule(function()
+                    api.nvim_win_set_cursor(winnr, { api.nvim_buf_line_count(bufnr), 0 })
+                end)
+            end
+        end)
+    end
 end
 
 return M
