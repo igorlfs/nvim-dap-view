116570ca3cfb64e2ebf96a72bd7858eedbd386b2
Igor <igorlfs@ufmg.br>
2025-09-10 01:49:00 -0300

    revert(docs): redirect to home

--- docs/src/routes/+page.server.ts
+++ docs/src/routes/+page.server.ts
@@ -1,6 +1,5 @@
-import { base } from '$app/paths';
 import { redirect, type Load } from '@sveltejs/kit';
 
 export const load: Load = async () => {
-    redirect(301, `${base}/home`)
+    redirect(301, 'home')
 };
