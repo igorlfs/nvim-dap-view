4056093959c038e0c5a3c5962038096d4d7c1a6e
Igor <igorlfs@ufmg.br>
2025-09-15 23:28:20 -0300

    chore(docs): fix redirect to home for real this time

--- docs/src/routes/+page.server.ts
+++ docs/src/routes/+page.server.ts
@@ -1,5 +1,6 @@
+import { base } from '$app/paths';
 import { redirect, type Load } from '@sveltejs/kit';
 
 export const load: Load = async () => {
-    redirect(301, 'home')
+    redirect(301, `${base}/home`)
 };
