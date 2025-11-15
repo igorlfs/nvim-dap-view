8799d323afd4e58463217327e5de25f276051443
Igor <igorlfs@ufmg.br>
2025-11-15 16:11:57 -0300

    fix: keep window proportions consistent

--- lua/dap-view/console/options.lua
+++ lua/dap-view/console/options.lua
@@ -1,3 +1,5 @@
+local setup = require("dap-view.setup")
+
 local M = {}
 
 ---@param winnr integer
@@ -8,10 +10,19 @@ M.set_win_options = function(winnr)
     win.wrap = false
     win.number = false
     win.relativenumber = false
-    win.winfixheight = true
     win.statuscolumn = ""
     win.foldcolumn = "0"
     win.winfixbuf = true
+
+    local pos = setup.config.windows.position
+
+    -- That's a bit confusing, but since the terminal split is just created from a top level split,
+    -- to keep the proportions consistent, we just have to follow what the top level split says
+    if pos == "above" or pos == "below" then
+        win.winfixheight = true
+    else
+        win.winfixwidth = true
+    end
 end
 
 return M
