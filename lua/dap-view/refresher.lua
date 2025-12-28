igorlfs/nvim-dap-view

commit d911b51ab3c79237e7ecd42bc9a619b167665095

refactor(scopes): native support

lua/dap-view/refresher.lua
@@ -1,9 +1,7 @@
 local state = require("dap-view.state")
 local threads = require("dap-view.threads.view")
-local scopes = require("dap-view.scopes.view")
 local exceptions = require("dap-view.exceptions.view")
 local eval = require("dap-view.watches.eval")
-local util = require("dap-view.util")
 
 local M = {}
 
@@ -11,7 +9,6 @@ local M = {}
 ---
 ---threads => may get outdated when finishing / switching sessions
 ---exceptions => no longer valid after finishing a session
----scopes => no longer valid after finishing a session
 ---sessions => obviously needs to be updated when switching / finishing a session
 M.refresh_session_based_views = function()
     if state.current_section == "threads" then
@@ -22,12 +19,6 @@ M.refresh_session_based_views = function()
         require("dap-view.views").switch_to_view("watches")
     elseif state.current_section == "sessions" then
         require("dap-view.views").switch_to_view("sessions")
-    end
-
-    if util.is_buf_valid(state.bufnr) then
-        if state.current_section == "scopes" then
-            scopes.refresh()
-        end
     end
 end
 

lua/dap-view/config.lua
@@ -98,7 +98,9 @@ M.config = {
                 label = "Scopes [S]",
                 short_label = "󰂥 [S]",
                 action = function()
-                    views.switch_to_view("scopes")
+                    coroutine.wrap(function()
+                        views.switch_to_view("scopes")
+                    end)()
                 end,
             },
             exceptions = {

lua/dap-view/highlight.lua
@@ -38,6 +38,7 @@ local define_base_links = function()
     hl_create("ControlDisconnect", "DapBreakpoint")
 
     hl_create("WatchExpr", "Identifier")
+    hl_create("WatchMore", "Comment")
     hl_create("WatchError", "DiagnosticError")
     hl_create("WatchUpdated", "DiagnosticVirtualTextWarn")
 

lua/dap-view/listeners.lua
@@ -2,7 +2,6 @@ local dap = require("dap")
 
 local state = require("dap-view.state")
 local breakpoints = require("dap-view.breakpoints.view")
-local scopes = require("dap-view.scopes.view")
 local util = require("dap-view.util")
 local term = require("dap-view.console.view")
 local setup = require("dap-view.setup")
@@ -109,15 +108,7 @@ end
 dap.listeners.after.scopes[SUBSCRIPTION_ID] = function(session)
     if state.current_section == "sessions" then
         require("dap-view.views").switch_to_view("sessions")
-    end
-
-    -- nvim-dap needs a buffer to operate
-    if util.is_buf_valid(state.bufnr) then
-        if state.current_section == "scopes" then
-            scopes.refresh()
-        end
-    end
-    if state.current_section == "threads" then
+    elseif state.current_section == "threads" then
         require("dap-view.views").switch_to_view("threads")
 
         if session.current_frame ~= nil and util.is_win_valid(state.winnr) then
@@ -128,6 +119,13 @@ dap.listeners.after.scopes[SUBSCRIPTION_ID] = function(session)
     -- Do not use `event_stopped`
     -- It may cause race conditions
     refresher.refresh_all_expressions()
+
+    if state.current_section == "scopes" then
+        -- We must wrap in a coroutine because we fetch variables on the fly
+        coroutine.wrap(function()
+            require("dap-view.views").switch_to_view("scopes")
+        end)()
+    end
 end
 
 local continue = { "event_continued", "continue" }

lua/dap-view/scopes/actions.lua
@@ -1,0 +1,18 @@
+local state = require("dap-view.state")
+
+local M = {}
+
+---@param lnum number
+M.expand_or_collapse = function(lnum)
+    local path = state.line_to_path[lnum]
+
+    if path then
+        state.expanded_variables[path] = not state.expanded_variables[path]
+
+        return true
+    else
+        vim.notify("Nothing to expand")
+    end
+end
+
+return M

lua/dap-view/scopes/view.lua
@@ -1,57 +1,161 @@
 local dap = require("dap")
-local dap_widgets = require("dap.ui.widgets")
 
 local views = require("dap-view.views")
-local winbar = require("dap-view.options.winbar")
 local state = require("dap-view.state")
 local util = require("dap-view.util")
-local widgets = require("dap-view.util.widgets")
+local hl = require("dap-view.util.hl")
 
 local M = {}
 
-local scopes_widget
+local api = vim.api
 
-local last_scopes_bufnr
+---@type dap.Session
+local session
 
-local launch_and_refresh_widget = function()
-    if last_scopes_bufnr == nil or last_scopes_bufnr ~= state.bufnr then
-        scopes_widget = widgets.new_widget(state.bufnr, state.winnr, dap_widgets.scopes)
-        last_scopes_bufnr = state.bufnr
+---@type integer
+local frame_id
 
-        scopes_widget.open()
+---Redrawing is jittery if we set lines on the fly
+---Prevent that by batching all buffer updates
+---Also need to handle concurrent calls, by creating multiple instances
+---@class dapview.Canvas
+---@field contents string[]
+---@field highlights [string, [integer,integer], [integer,integer]][][]
+
+---@param variables_reference integer
+---@param parent_path string
+---@param line integer
+---@param depth integer
+---@param canvas dapview.Canvas
+local function show_variables(variables_reference, parent_path, line, depth, canvas)
+    local err, response = session:request(
+        "variables",
+        { variablesReference = variables_reference, context = "variables", frameId = frame_id }
+    )
+
+    if err then
+        local err_content = string.rep("\t", depth + 1) .. tostring(err)
+
+        canvas.contents[#canvas.contents + 1] = err_content
+
+        canvas.highlights[#canvas.highlights + 1] = { { "WatchError", { line, 0 }, { line, #err_content } } }
+
+        line = line + 1
+
+        return line
     end
 
-    -- Always refresh, otherwise outdated information may be shown
-    scopes_widget.refresh()
+    local variables = response and response.variables or {}
 
-    local session = dap.session()
+    for _, variable in pairs(variables) do
+        local show_expand_hint = #variable.value == 0 and variable.variablesReference > 0
+        local value = show_expand_hint and "..." or variable.value
+        local variable_name = variable.name
+        local content = variable_name .. " = " .. value
 
-    -- We need to ensure that the window is valid before calling `cleanup_view`
-    -- One scenario where it can invalid is when switching tabs: the window is valid for the original tab
-    -- but not for the new one
-    if not util.is_win_valid(state.winnr) then
-        return
+        -- Can't have linebreaks with nvim_buf_set_lines
+        local trimmed_content = content:gsub("%s+", " ")
+
+        local indented_content = string.rep("\t", depth) .. trimmed_content
+
+        canvas.contents[#canvas.contents + 1] = indented_content
+
+        hl.hl_range("WatchExpr", { line, depth }, { line, depth + #variable.name })
+
+        local path = parent_path .. "." .. variable.name
+
+        local prev_variable_value = state.variable_values[path]
+
+        local updated = prev_variable_value and prev_variable_value ~= variable.value
+
+        state.variable_values[path] = variable.value
+
+        local type_hl_group = (updated and "WatchUpdated")
+            or (show_expand_hint and "WatchMore")
+            or (variable.type and hl.types_to_hl_group[variable.type:lower()])
+
+        canvas.highlights[#canvas.highlights + 1] = {
+            { "WatchExpr", { line, depth }, { line, depth + #variable.name } },
+            type_hl_group and { type_hl_group, { line, #variable_name + 3 + depth }, { line, -1 } } or nil,
+        }
+
+        line = line + 1
+
+        state.line_to_path[line] = path
+
+        if state.variable_path_is_expanded[path] and variable.variablesReference > 0 then
+            line = show_variables(variable.variablesReference, path, line, depth + 1, canvas)
+        end
     end
 
-    if views.cleanup_view(not session, "No active session") then
-        return
-    end
-
-    ---@cast session dap.Session
-
-    local has_scopes = session.current_frame and session.current_frame.scopes and #session.current_frame.scopes > 0
-
-    -- We can't count the number of lines here because the widget might not have refreshed yet,
-    -- so we resort to looking at the scopes directly
-    views.cleanup_view(not has_scopes, "Debug adapter returned no scopes")
+    return line
 end
 
 M.show = function()
-    winbar.refresh_winbar("scopes")
+    if util.is_buf_valid(state.bufnr) and util.is_win_valid(state.winnr) then
+        local tmp_session = dap.session()
 
-    launch_and_refresh_widget()
+        if views.cleanup_view(tmp_session == nil, "No active session") then
+            return
+        end
+
+        ---@cast tmp_session dap.Session
+
+        session = tmp_session
+
+        local current_frame = session.current_frame
+
+        if views.cleanup_view(current_frame == nil, "Session not stopped") then
+            return
+        end
+
+        ---@cast current_frame dap.StackFrame
+
+        frame_id = current_frame.id
+
+        local frame_scopes = current_frame.scopes
+
+        local is_empty = frame_scopes == nil or vim.tbl_isempty(frame_scopes)
+
+        if views.cleanup_view(is_empty, "No scopes for the current frame") then
+            return
+        end
+
+        ---@cast frame_scopes dap.Scope[]
+
+        ---@type dap.Scope[]
+        local filtered_scopes = {}
+        for _, scope in ipairs(frame_scopes) do
+            if not scope.expensive then
+                table.insert(filtered_scopes, scope)
+            end
+        end
+
+        local line = 0
+
+        ---@type dapview.Canvas
+        local canvas = { contents = {}, highlights = {} }
+
+        for _, scope in ipairs(filtered_scopes) do
+            canvas.contents[#canvas.contents + 1] = scope.name
+
+            canvas.highlights[#canvas.highlights + 1] = { { "Thread", { line, 0 }, { line, -1 } } }
+
+            line = line + 1
+
+            line = show_variables(scope.variablesReference, scope.name, line, 1, canvas)
+        end
+
+        api.nvim_buf_set_lines(state.bufnr, 0, line - 1, false, canvas.contents)
+
+        for _, highlights in ipairs(canvas.highlights) do
+            for _, highlight in ipairs(highlights) do
+                hl.hl_range(highlight[1], highlight[2], highlight[3])
+            end
+        end
+
+        api.nvim_buf_set_lines(state.bufnr, line, -1, true, {})
+    end
 end
-
-M.refresh = launch_and_refresh_widget
 
 return M

lua/dap-view/state.lua
@@ -34,8 +34,12 @@
 ---@field threads_error? string
 ---@field frames_by_line table<integer, dap.StackFrame>
 ---@field expression_views_by_line table<integer, {expression: string, view: dapview.ExpressionView}>
----@field variable_views_by_line table<integer, {parent_reference: number, variable: dap.Variable, view: dapview.VariableView}>
+---@field variable_views_by_line table<integer, {parent_reference: number, view: dapview.VariableView}>
 ---@field sessions_by_line table<integer, dap.Session>
+---@field variable_views_by_evaluate_name table<string, dapview.VariableView>
+---@field variable_values table<string,string>
+---@field expanded_variables table<string,boolean>
+---@field line_to_path table<integer,string>
 ---@field watched_expressions table<string, dapview.ExpressionView>
 ---@field expr_count integer
 ---@field cur_pos table<dapview.DefaultSection,integer?>
@@ -51,6 +55,10 @@ local M = {
     sessions_by_line = {},
     subtle_frames = false,
     watched_expressions = {},
+    variable_views_by_evaluate_name = {},
+    expanded_variables = {},
+    variable_values = {},
+    line_to_path = {},
     cur_pos = {},
 }
 

lua/dap-view/util/hl.lua
@@ -23,4 +23,19 @@ M.highlight_file_name_and_line_number = function(row, len_path, len_lnum)
     M.hl_range("Separator", { row, lnum_end }, { row, lnum_end + 1 })
 end
 
+M.types_to_hl_group = {
+    boolean = "Boolean",
+    bool = "Boolean",
+    str = "String",
+    string = "String",
+    int = "Number",
+    long = "Number",
+    number = "Number",
+    double = "Float",
+    float = "Float",
+    -- debugpy's "None"
+    nonetype = "Constant",
+    ["function"] = "Function",
+}
+
 return M

commit afa19220bfad05322f92806ea24c8dc4f7753a37

fix: refresh scopes when terminating session
A regression from d911b51a

lua/dap-view/refresher.lua
@@ -9,6 +9,7 @@ local M = {}
 ---
 ---threads => may get outdated when finishing / switching sessions
 ---exceptions => no longer valid after finishing a session
+---scopes => no longer valid after finishing a session
 ---sessions => obviously needs to be updated when switching / finishing a session
 M.refresh_session_based_views = function()
     if state.current_section == "threads" then
@@ -19,6 +20,10 @@ M.refresh_session_based_views = function()
         require("dap-view.views").switch_to_view("watches")
     elseif state.current_section == "sessions" then
         require("dap-view.views").switch_to_view("sessions")
+    elseif state.current_section == "scopes" then
+        coroutine.wrap(function()
+            require("dap-view.views").switch_to_view("scopes")
+        end)()
     end
 end
 
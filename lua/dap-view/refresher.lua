afa19220bfad05322f92806ea24c8dc4f7753a37
Igor <igorlfs@ufmg.br>
2025-12-28 20:44:31 -0300

    fix: refresh scopes when terminating session
    A regression from d911b51a

--- lua/dap-view/refresher.lua
+++ lua/dap-view/refresher.lua
@@ -9,6 +9,7 @@ local M = {}
 ---
 ---threads => may get outdated when finishing / switching sessions
 ---exceptions => no longer valid after finishing a session
+---scopes => no longer valid after finishing a session
 ---sessions => obviously needs to be updated when switching / finishing a session
 M.refresh_session_based_views = function()
     if state.current_section == "threads" then
@@ -19,6 +20,10 @@ M.refresh_session_based_views = function()
         require("dap-view.views").switch_to_view("watches")
     elseif state.current_section == "sessions" then
         require("dap-view.views").switch_to_view("sessions")
+    elseif state.current_section == "scopes" then
+        coroutine.wrap(function()
+            require("dap-view.views").switch_to_view("scopes")
+        end)()
     end
 end
 
