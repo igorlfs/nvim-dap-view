2b8870b08ea50aafea23cfc60fb1f612d7cc2726
Igor <igorlfs@ufmg.br>
2026-01-30 22:27:25 -0300

    fix(threads): handle out of bounds cursor position

--- lua/dap-view/threads/init.lua
+++ lua/dap-view/threads/init.lua
@@ -6,9 +6,14 @@ local api = vim.api
 
 ---@param current_frame_id number
 M.track_cursor_position = function(current_frame_id)
+    local line_count = api.nvim_buf_line_count(state.bufnr)
+
     for k, v in pairs(state.frames_by_line) do
         if v.id == current_frame_id then
-            api.nvim_win_set_cursor(state.winnr, { k, 0 })
+            -- Sometimes vscode-js-debug goes wacko mode and may trigger a tracking with a non-existent position
+            if k <= line_count then
+                api.nvim_win_set_cursor(state.winnr, { k, 0 })
+            end
         end
     end
 end
